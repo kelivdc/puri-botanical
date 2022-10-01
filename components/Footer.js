83ea19382836b7c630e7a88d357d6e1218a1607c
Arie Ardiansyah <kelivdc@yahoo.com>
2022-10-02 01:27:40 +0700

    Add copyright

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <Box bgcolor="#000" color="#fff" p={2} marginBottom={8}>
         <Typography align="center" color="#ccc">
-            Copyright &copy 2022. All rights reserved
+            Copyright ©2022. All rights reserved
         </Typography>
     </Box>
   )
